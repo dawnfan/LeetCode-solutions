1ef4c1d87d94ccd872a58dcc0ea2e4edc2c7cfbf
Jiajia Fan <dawnfan@users.noreply.github.com>
2018-10-24 22:16:51 +0800

    reload

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -827,7 +828,7 @@ public:
 		return out_num;
 	}
 
-	ListNode* reverseNode(ListNode*node, ListNode* pre_node){
+	ListNode* reverseNode(ListNode*node, ListNode* pre_node) {
 		if (node == NULL)	return pre_node;
 		ListNode* next_n = node->next;
 		node->next = pre_node;
@@ -892,7 +893,7 @@ public:
 		return out_inter;
 	}
 
-	int heightAndDiameter(TreeNode* root, int & cur_d){
+	int heightAndDiameter(TreeNode* root, int & cur_d) {
 		if (root == NULL) return 0;
 		int left_h = heightAndDiameter(root->left, cur_d);
 		int right_h = heightAndDiameter(root->right, cur_d);
@@ -909,7 +910,7 @@ public:
 		string out_s;
 		bool isPositive = num < 0 ? false : true;
 		int t = abs(num);
-		while (t > 0){
+		while (t > 0) {
 			out_s = to_string(t % 7) + out_s;
 			t /= 7;
 		}
@@ -983,7 +984,7 @@ public:
 			}
 			else
 			{
-				pre_sub = temp_sub[s[i]] > pre_sub? temp_sub[s[i]]:pre_sub;
+				pre_sub = temp_sub[s[i]] > pre_sub ? temp_sub[s[i]] : pre_sub;
 				out_num = max(out_num, i - pre_sub);
 				temp_sub[s[i]] = i;
 			}
@@ -1008,7 +1009,7 @@ public:
 			else if (j > n) { int curind = toStop - j; curnum = nums1[curind]; prenum = (curind - 1 > i) ? nums1[curind - 1] : prenum; break; }
 			else if (nums1[i] > nums2[j])
 			{
-				curnum = nums2[j];	j++;	
+				curnum = nums2[j];	j++;
 			}
 			else
 			{
@@ -1024,11 +1025,14 @@ public:
 		return out_median;
 	}
 
+	//5. Longest Palindromic Substring
 	string longestPalindrome_(string s) {
 		string out_sub = "";
+		if (s.size() == 0) return out_sub;
 		int maxlen = 0;
 		int subpos = -1;
-		int dpsub[1000][1000] = { 0 };
+		// the stack can overflow if not 'static'.
+		static int dpsub[1000][1000] = { 0 };
 		for (int i = 0; i < s.size(); i++)
 		{
 			for (int j = 0; j < s.size(); j++)
@@ -1045,28 +1049,140 @@ public:
 					}
 					if (maxlen < dpsub[i][j])
 					{
-						maxlen = dpsub[i][j];
-						if (j == i - maxlen + 1)
-						{
+						// limit the position  we find, since the reverse one should be the relative
+						if ((j - dpsub[i][j] + 1) == (s.size() - 1 - i)) {
+							maxlen = dpsub[i][j];
 							subpos = i;
 						}
 					}
 				}
 			}
 		}
-		if (subpos == -1)
-		{
-			out_sub = s[0];
-		}
-		else out_sub = s.substr(subpos - maxlen + 1, maxlen);
+		out_sub = s.substr(subpos - maxlen + 1, maxlen);
 		return out_sub;
 	}
 
+	//856. Score of Parentheses
 	int scoreOfParentheses(string S) {
-		int out_score;
+		// tip: find the pattern how actions is pushed into the stack
+		int out_score = 0;
+		stack<int> actionlist;
 
-
+		int val = 0;
+		for (int i = 0; i < S.size() - 1; i++)
+		{
+			if (S.substr(i, 2) == "((")
+			{
+				actionlist.push(2); // * 2
+			}
+			else if (S.substr(i, 2) == "()")
+			{
+				actionlist.push(1); // 1
+			}
+			else if (S.substr(i, 2) == ")(")
+			{
+				if (actionlist.top() != 0)
+					actionlist.push(0); // +
+			}
+			else if (S.substr(i, 2) == "))")
+			{
+				int cur = actionlist.top();
+				actionlist.pop();
+				val += cur;
+				do
+				{
+					cur = actionlist.top();
+					actionlist.pop();
+					if (cur == 0)
+					{
+						val += actionlist.top();
+						actionlist.pop();
+						//cur = actionlist.top();
+						//actionlist.pop();
+					}
+				} while (cur != 2);
+				val *= cur;
+				actionlist.push(val);
+				//actionlist.push(0);
+				val = 0;
+			}
+		}
+		while (!actionlist.empty())
+		{
+			out_score += actionlist.top();
+			actionlist.pop();
+		}
 		return out_score;
+	}
+	int helper(const string& S, int l, int r)
+	{
+		if (r - l == 1) return 1;
+		int b = 0;
+		for (int i = l; i < r; ++i)
+		{
+			if (S[i] == '(') b++;
+			if (S[i] == ')') b--;
+			if (b == 0)
+			{
+				return helper(S, l, i) + helper(S, i + 1, r);
+			}
+		}
+		return 2 * helper(S, l + 1, r - 1);
+	}
+	int scoreOfParentheses2(string S) {
+		// recursive, directly using division.
+		return helper(S, 0, S.size() - 1);
+	}
+
+	//6. ZigZag Conversion
+	string convert(string s, int numRows) {
+		string out_str = "";
+		if (s.size() <= numRows || numRows < 2)
+		{
+			return s;
+		}
+		int warpnum = (numRows + numRows - 2);
+		int colnum_o = s.size() / warpnum;
+		int remainnum = s.size() % warpnum;
+		int colnum = remainnum > 0 ? colnum_o + 1 : colnum_o;
+		// encode the string
+		for (int i = 0; i < colnum; i++)
+		{
+			int cur_pre = i * (warpnum + (numRows - 2)*(numRows - 1)) + numRows;
+			for (int t = 0; t < numRows - 1; t++)
+			{
+				int curind = cur_pre + t*(numRows - 1);
+				if (curind > (s.size() - 1))
+				{
+					break;
+				}
+				s.insert(curind, numRows - 2, ' ');
+			}
+		}
+		s.append(numRows, ' ');
+		// decode the string
+		int warpcolnum = colnum_o*(numRows - 1);
+		if (remainnum > numRows)
+		{
+			warpcolnum += remainnum - numRows + 1;
+		}
+		else if (remainnum > 0)
+		{
+			warpcolnum += 1;
+		}
+		for (int i = 0; i < numRows; i++)
+		{
+			for (int j = 0; j < warpcolnum; j++)
+			{
+				int curind = j*numRows + i;
+				if (s[curind] != ' ')
+				{
+					out_str.append(s, curind, 1);
+				}
+			}
+		}
+
+		return out_str;
 	}
 };
 
@@ -1135,8 +1251,8 @@ int main() {
 	//ListNode* l2 = new ListNode(2);
 	//cout << leetcode.addTwoNumbers(l1, l2)->val << endl;
 
-	string tt = "ynyo";
-	cout << leetcode.lengthOfLongestSubstring(tt) << endl;
+	string tt = leetcode.convert("ABCD", 3);
+	cout << tt << endl;
 
 	return 0;
 }
